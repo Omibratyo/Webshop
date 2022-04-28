3385e09f381fc90e31923c56e19c791fde4a9b1a
Egri Bálint Márk <egri767@gmail.com>
2022-04-28 14:03:30 +0200

    new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'webshop-46282',
+    appId: '1:909494766348:web:6063e38d23f13f72719729',
+    storageBucket: 'webshop-46282.appspot.com',
+    apiKey: 'AIzaSyBv_eQE8My9aZf1OUTJqSWHeIh0bJHDQ2Y',
+    authDomain: 'webshop-46282.firebaseapp.com',
+    messagingSenderId: '909494766348',
+    measurementId: 'G-FB9MRLLDPN',
+  },
   production: false
 };
 
