ba445c92854e32f2cf0f872735ff6111e7f8c9d0
Egri Bálint Márk <egri767@gmail.com>
2022-04-29 18:02:56 +0200

    useless

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { GoogleAuthProvider } from 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat';
+import { ActivatedRoute, Router } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(
-    public afAuth: AngularFireAuth // Inject Firebase auth service
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
+    public route: ActivatedRoute,
+    public router: Router
   ) {}
   // Sign in with Google
   GoogleAuth() {
@@ -19,6 +22,8 @@ export class AuthService {
       .signInWithPopup(provider)
       .then((result) => {
         console.log('You have been successfully logged in!');
+        //let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+        this.router.navigate(['']);
       })
       .catch((error) => {
         console.log(error);
